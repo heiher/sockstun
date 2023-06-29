681c5e3c2f1e86aa97a56719f84eaf91b0b58ec3
ziqi mo <709847739@qq.com>
2023-06-29 18:08:26 +0800

    适配android31和支持模拟器

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -15,7 +15,7 @@
 
 APP_OPTIM := release
 APP_PLATFORM := android-21
-APP_ABI := armeabi-v7a arm64-v8a
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 APP_CFLAGS := -O3 -DPKGNAME=hev/sockstun
 APP_CPPFLAGS := -O3 -std=c++11
 NDK_TOOLCHAIN_VERSION := clang
